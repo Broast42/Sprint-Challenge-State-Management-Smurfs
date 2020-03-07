2978adb0b6187f477644d237fcd2efdc96c0555b
rdowd <rjarrettdowd@yahoo.com>
2020-03-06 21:48:51 -0500

    can now edit smurfs.

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState }from 'react';
+import EditForm from './EditForm';
 
 const SmurfCard = (props) => {
+    
+    const [showForm, setShowForm] = useState(false);
+    const toggleForm = () =>{
+        setShowForm(!showForm);
+    }
     return(
         <div className="card" >
             <h2>{props.data.name}</h2>
             <h3>Age: {props.data.age}</h3>
             <h3>Height: {props.data.height}</h3>
+            <button onClick={()=> toggleForm()}>Edit</button>
+            {showForm === true ? <EditForm data={props.data}/> : ""}
         </div>
     );
 };
